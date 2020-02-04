a9fe3fd631dbe8fd6393babe79174d6cae3b9aa3
stefan judis <stefanjudis@gmail.com>
2020-02-04 11:46:04 +0100

    Fix jq bug binary problem

--- bin/create-phone-number-forwarding.sh
+++ bin/create-phone-number-forwarding.sh
@@ -8,7 +8,7 @@
 # https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # extend path to access jq, twilio-run, the Twilio CLI, ...
-export PATH="$PATH:$CURRENT_DIR/../node_modules/.bin"
+export PATH="$PATH:$CURRENT_DIR/../node_modules/.bin:$CURRENT_DIR/../node_modules/node-jq/bin"
 
 ################################################################################
 
